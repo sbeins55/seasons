1ee0cbb97b5de85813340de9740cb7d97395e739
sbeins <sean.beins@gmail.com>
2019-03-14 15:51:59 -0500

    Add Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 
 const App = () => {
   return (
-      <div>Hello World!</div>
+      <SeasonDisplay/>
   );
 };
 
