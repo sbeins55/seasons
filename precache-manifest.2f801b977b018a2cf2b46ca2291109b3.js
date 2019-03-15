94e36d1887349d428b710e337128c95f1c5f03be
sbeins <sean.beins@gmail.com>
2019-03-15 13:25:24 -0500

    Updates

---
+++ precache-manifest.2f801b977b018a2cf2b46ca2291109b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a19fdc4473a0873f16",
+    "url": "/seasons/static/css/main.cc680ba3.chunk.css"
+  },
+  {
+    "revision": "35a19fdc4473a0873f16",
+    "url": "/seasons/static/js/main.5600ffbc.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  },
+  {
+    "revision": "49e8ed93f0c971c12a69",
+    "url": "/seasons/static/js/2.11b5b539.chunk.js"
+  },
+  {
+    "revision": "9d36584de7e7c80efbe7878d92bb7340",
+    "url": "/seasons/index.html"
+  }
+];
